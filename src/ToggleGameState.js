168b7a6a498625bac795e87a127324121114c59a
Karis Lewis <karis.lewis@bison.howard.edu>
2024-12-10 14:42:22 +0000

    Force overwrite remote repository

---
+++ src/ToggleGameState.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import { GAME_STATE } from './GameState.js';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Select from '@material-ui/core/Select';
+import FormControl from '@material-ui/core/FormControl';
+import './ToggleGameState.css';
+
+function ToggleGameState({ gameState, setGameState, setSize, setTotalTime }) {
+  const [buttonText, setButtonText] = useState('Start a new game!');
+  const [startTime, setStartTime] = useState(0);
+  let deltaTime;
+
+  function updateGameState(endTime) {
+    if (gameState === GAME_STATE.BEFORE || gameState === GAME_STATE.ENDED) {
+      setStartTime(Date.now());
+      setGameState(GAME_STATE.IN_PROGRESS);
+      setButtonText('End game');
+    } else if (gameState === GAME_STATE.IN_PROGRESS) {
+      deltaTime = (endTime - startTime) / 1000.0;
+      setTotalTime(deltaTime);
+      setGameState(GAME_STATE.ENDED);
+      setButtonText('Start a new game!');
+    }
+  }
+
+  const handleChange = (event) => {
+    setSize(event.target.value);
+  };
+
+  return (
+    <div className="Toggle-game-state">
+      <Button variant="outlined" onClick={() => updateGameState(Date.now())}>
+        {buttonText}
+      </Button>
+
+      {(gameState === GAME_STATE.BEFORE || gameState === GAME_STATE.ENDED) && (
+        <div className="Input-select-size">
+          <FormControl>
+            <Select
+              labelId="sizelabel"
+              id="sizemenu"
+              onChange={handleChange}
+            >
+              <MenuItem value={4}>4x4</MenuItem>
+              <MenuItem value={6}>6x6</MenuItem>
+              <MenuItem value={8}>8x8</MenuItem>
+            </Select>
+            <FormHelperText>Choose game size</FormHelperText>
+          </FormControl>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ToggleGameState;
